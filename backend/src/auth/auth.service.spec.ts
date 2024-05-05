86e416dede3ad53a2f9b15a512a58ba1e0aa6b17
Harish Ramamurthy <harish.ramamurthy@yahoo.in>
2024-05-05 09:44:23 +0100

    update verify valid signature test

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,10 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { Wallet } from 'ethers';
 
 describe('AuthService', () => {
   let service: AuthService;
-  const signature =
-    '0x020d671b80fbd20466d8cb65cef79a24e3bca3fdf82e9dd89d78e7a4c4c045bd72944c20bb1d839e76ee6bb69fed61f64376c37799598b40b8c49148f3cdd88a1b';
-  const ethAddress = '0xf924e868F24F47804F0c79015DF5571EC98f820F';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,8 +15,11 @@ describe('AuthService', () => {
   });
 
   it('should verify valid signature', async () => {
+    const wallet = Wallet.createRandom();
+    const message = 'Signup';
+    const signature = await wallet.signMessage(message);
     const address = await service.signUp(signature);
-    expect(address).toEqual(ethAddress);
+    expect(address).toEqual(wallet.address);
   });
 
   it('should throw BadRequestException for invalid signature', async () => {
